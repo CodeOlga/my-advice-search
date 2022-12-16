cac53752c11ab3b4698bb821d998d55c51ff6e51
CodeOlga <111949555+CodeOlga@users.noreply.github.com>
2022-12-16 12:02:09 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import MyAdviceComponent from './MyAdviceComponent';
+
 
 function App() {
+
+  // const MY_ID = "";
+  // const MY_KEY = "";
+
+  const [mySearch, setMySearch] = useState('');
+  const [myAdvice, setMyAdvice] = useState('');
+
+  useEffect (() => {
+    const getAdvice = async() => {
+      const response = await fetch (`https://www.boredapi.com/api/activity/`);
+      const data = await response.json();
+      console.log(data)
+      setMyAdvice(data.activity)
+    }
+    getAdvice();
+  }, [])
+
+  const myAdviceSearch = (e) => {
+    console.log(e.target.value)
+    setMySearch(e.target.value)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <p>ADVICE</p>
+      </div>
+
+      <div className="container">
+        <form>
+          <input className='search' type="text" placeholder='Search...' onChange={myAdviceSearch} value={mySearch}/>
+          <div className="container">
+            <button>Click</button>
+          </div>
+        </form>
+      </div>
+
+      {myAdvice => (
+        <MyAdviceComponent/>
+      )} 
+      
+
     </div>
+  
   );
 }
 
---
+++ src/MyAdviceComponent.js
@@ -0,0 +1,8 @@
+function MyAdviceComponent() {
+  return(
+    <div>
+      <h2>test</h2>
+    </div>
+  )
+}
+export default MyAdviceComponent;
